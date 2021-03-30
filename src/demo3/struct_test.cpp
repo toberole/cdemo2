a47cc8fae832e984f62c7637ecc0cec9a15916e2
zhouwei441300 <zhouwei441300@163.com>
2021-03-30 16:23:55 +0800

    note

---
+++ src/demo3/struct_test.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+struct websocket_frame
+{
+    uint8_t fin : 1;
+
+    uint8_t rsv1 : 1;
+    uint8_t rsv2 : 1;
+    uint8_t rsv3 : 1;
+
+    uint8_t opcode : 4;
+
+    uint8_t mask : 1;
+
+    uint8_t payload_len : 7;
+};
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "websocket_frame size: " << sizeof(websocket_frame) << std::endl;
+
+    uint16_t i = 33679;
+    websocket_frame f;
+    f.fin = (uint8_t)131;
+    
+    std::cout << "f.fin: " << (f.fin&((uint8_t)4)) << std::endl;
+    return 0;
+}
