def70655a4becd33c832caa31984508fb72d163d
zhouwei441300 <zhouwei441300@163.com>
2021-08-04 16:41:31 +0800

    note

---
+++ test-demo/demo7.cpp
@@ -0,0 +1,55 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <atomic>
+
+struct demo7
+{
+    int i = 0;
+    int j = 0;
+};
+
+void test1()
+{
+    demo7 d{1, 2};
+    printf("i: %d,j: %d\n", d.i, d.j);
+
+    std::string s = "Hello";
+    int i = 0;
+    switch (i)
+    {
+    case 0:
+
+        break;
+
+    default:
+        break;
+    }
+}
+
+void test2(char **p)
+{
+    *p = new char[100 + 1];
+    for (size_t i = 0; i < 100; i++)
+    {
+        (*p)[i] = 'a';
+    }
+}
+
+void test3()
+{
+    register int i = 0;
+    printf("i: %d\n", i);
+    std::atomic_uint ii = 0;
+    std::atomic<long> l = 0;
+}
+
+int main(int argc, char const *argv[])
+{
+
+    char *p = nullptr;
+    test2(&p);
+    printf("%s\n", p);
+    test3();
+    return 0;
+}
---
+++ test-demo/demo8.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+class demo8
+{
+private:
+    int i = 0;
+
+public:
+    demo8(int i) : i(i)
+    {
+    }
+    ~demo8()
+    {
+    }
+
+    demo8(demo8 &&d)
+    {
+        printf("move demo8 ......");
+    }
+
+    friend void test(demo8 d)
+    {
+        printf("demo8 i: %d", d.i);
+    }
+};
+
+void test(){
+    demo8 d;
+    demo8 d1 = std::move(d);
+}
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
