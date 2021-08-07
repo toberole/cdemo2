0c573c9144663cb3ed80f41934bdc09f7d6b544d
zhouwei441300 <zhouwei441300@163.com>
2021-08-07 18:33:51 +0800

    notwe

---
+++ test-demo/demo10.cpp
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
--- test-demo/demo9.cpp
+++ test-demo/demo9.cpp
@@ -1,10 +1,32 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
+#include <vector>
+#include <list>
+
+void test()
+{
+    std::vector<int> v;
+    int i = 0;
+    int &ii = i;
+    v.push_back(ii);
+
+    std::list<int> l;
+    l.push_back(1);
+    l.push_front(0);
+    l.push_back(2);
+    std::list<int>::iterator itr = l.begin();
+    for (std::list<int>::iterator itr = l.begin(); itr != l.end(); itr++)
+    {
+        printf("%d\n", *itr);
+    }
+
+    l.pop_back();
+}
 
 int main(int argc, char const *argv[])
 {
 
     printf("demo9 ......\n");
-
+    test();
     return 0;
 }
