1955e1a4b16a4e0bfc50de2764483bc601aa3716
zhouwei441300 <zhouwei441300@163.com>
2021-08-19 19:50:26 +0800

    note

---
+++ test-demo/demo12.cpp
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+
+struct A
+{
+    char c;
+    short i1;
+    int i2;
+};
+
+struct B
+{
+    char c;
+    int i1;
+    short i2;
+};
+
+int main(int argc, char const *argv[])
+{
+    printf("A size:%d,B size: %d\n",sizeof(A),sizeof(B));
+    return 0;
+}
